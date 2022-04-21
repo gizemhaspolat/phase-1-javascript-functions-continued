c22ffba8bbabe9561a7c40b454b3d95cf3fd5357
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-04-21 19:35:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(sign = "*") {
+  function innerWrap(adj = "special") {
+    let stringAdj = `You are ${sign}` + `${adj}` + `${sign}!`;
+    return stringAdj;
+  }
+  return innerWrap;
+}
